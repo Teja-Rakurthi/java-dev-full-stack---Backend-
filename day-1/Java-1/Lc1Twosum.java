c226e3ea5a1fa6187dc0f32a2d9d78e3e3a12736
Teja-Rakurthi <tejarakurthi@gmail.com>
2026-05-27 22:40:00 +0530

    day-1 updated

--- day-1/Java-1/Lc1Twosum.java
+++ day-1/Java-1/Lc1Twosum.java
@@ -19,25 +19,29 @@
 
 import java.util.HashMap;
 public class  Lc1Twosum{
-    public static void Twosum(int[] nums,int target){
-        int diff;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            diff=target-nums[i];
-            if(map.containsKey(diff)){
-                System.out.println(i);
-                System.out.println(map.get(diff));
-                return;
-            }else{
-                map.put(nums[i], i);
+
+        public static int[] twoSum(int[] nums, int target) {
+            HashMap<Integer,Integer> map = new HashMap<>();
+            int[] arr = new int[2];
+            for (int i =0; i<nums.length; i++){
+                if( map.containsKey(target-nums[i])){
+                    arr[0]=map.get(target-nums[i]);
+                    arr[1]=i;
+                }else{
+                    map.put(nums[i],i);
+                }
             }
+            return arr;
         }
-    }
+
 
     public static void main(String[] args){
     int[] nums= {2,7,11,15};
     int target=9;
-    Twosum(nums,target);
+   int[] result= twoSum(nums,target);
+   for(int i=0; i<result.length;i++){
+       System.out.println(result[i]);
+   }
     }
 
 }
