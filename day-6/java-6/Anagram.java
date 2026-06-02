bab8dcd284b27caae38a585adf23a0587d2dcb06
Teja-Rakurthi <tejarakurthi@gmail.com>
2026-06-02 21:53:48 +0530

    day 6 updted with delay

---
+++ day-6/java-6/Anagram.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class SolutionAna{
+    public boolean isAnagram(String s, String t) {
+
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
+public class Anagram {
+    public static void main(String[] args){
+        String s="anagram";
+        String t="nagaram";
+        SolutionAna sol = new SolutionAna();
+        if(sol.isAnagram(s,t)){
+            System.out.println("yes anagram ra chintu");
+        }else{
+            System.out.println("anagram kadhura chintu");
+
+        }
+    }
+
+}
